cbc9a736d8b7bf14eb4860881cd8c959a14726fa
minhnguyen <minhdungnguyen2003@gmail.com>
2026-05-14 17:30:21 +0700

    add ver 48 snake

---
+++ assets/animations/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///animations.js",["./BezierEasing-8cd180b4.js","cc"],(function(){var e;return{setters:[null,function(n){e=n.cclegacy}],execute:function(){e._RF.push({},"a2651Re8vFB0peDRVGW4PJN","BubbleInOut",void 0),e._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/animations', 'chunks:///animations.js'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
